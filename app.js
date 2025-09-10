20e78a6fdddb7ec507cb19ccea2e63c0af2cd96e
Matheus de Carvalho <matheusdecarvalho100@gmail.com>
2025-09-10 12:05:22 -0300

    Dia 10/09/2025

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const port = 3000;
 // Middleware para analisar o corpo das requisições em JSON 
 app.use(express.json()); 
 // Importando as rotas do cliente 
-const clienteRoutes = require('./routes/clienteRoutes'); 
+const clienteRoutes = require('./routes/baladaRoutes'); 
 // Usando as rotas do cliente com o prefixo '/clientes' 
-app.use('/clientes', clienteRoutes); 
+app.use('/baladas', clienteRoutes); 
 // Iniciando o servidor na porta especificada 
 app.listen(port, () => { 
 console.log(`Servidor rodando em http://localhost:${port}`); 
